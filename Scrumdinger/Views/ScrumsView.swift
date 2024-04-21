e1b6cfa996a9100b1cc134df3311db544b83578f
SebastinGoh <gmcsebastin@gmail.com>
2024-04-22 00:17:52 +0800

    Initial commit with current progress with some styling and structure

---
+++ Scrumdinger/Views/ScrumsView.swift
@@ -0,0 +1,22 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Sebastin on 21/4/24.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    var body: some View {
+        List(scrums) { scrum in
+            CardView(scrum: scrum)
+                .listRowBackground(scrum.theme.mainColor)
+        }
+    }
+}
+
+#Preview {
+    ScrumsView(scrums: DailyScrum.sampleData)
+}
